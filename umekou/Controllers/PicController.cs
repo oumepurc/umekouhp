f2a5c21a6158ffcda4bfa58ea4ef4cece19007c7
oumepurc <oume.purc@gmail.com>
2015-10-13 22:50:22 +0900

    201510今月の一枚

---
+++ umekou/Controllers/PicController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace umekou.Controllers
+{
+    public class PicController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+
+
+    }
+
+   
+}
